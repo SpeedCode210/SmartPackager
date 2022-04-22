73cb1c6531e2a53df4848a5eba1a3ad5d5d3d533
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2022-04-22 21:26:05 +0100

    Update paths.py

--- paths.py
+++ paths.py
@@ -1,6 +1,8 @@
 import os
 
-LINUX_BINARIES = os.getcwd() + '/linux-bin'
-WINDOWS_BINARIES = os.getcwd() + '/windows-bin'
-TMP = os.getcwd() + '/tmp'
-BUILD = os.getcwd() + '/build'
+_dir = os.getcwd()
+
+LINUX_BINARIES = _dir + '/linux-bin'
+WINDOWS_BINARIES = _dir + '/windows-bin'
+TMP = _dir + '/tmp'
+BUILD = _dir + '/build'
